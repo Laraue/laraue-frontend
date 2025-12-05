cf4d8e654519525be4567baddf529b34e0b900c8
DESKTOP-KT6GSMQ\Ilya <win7user10@yandex.ru>
2025-12-05 10:54:29 +0300

    update version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-12-04',
+  compatibilityDate: '2025-12-05',
   devtools: { enabled: true },
   modules: ['@element-plus/nuxt', '@vesp/nuxt-fontawesome', 'nuxt-gtag'],
   elementPlus: {
