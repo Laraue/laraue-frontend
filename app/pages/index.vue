48d6e02c147d696bfdf22ca7f24a12148d3542c9
Ilya Belyansky <win7user10@yandex.ru>
2026-03-01 17:54:24 +0300

    example of translating for the most amount of cases

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -15,29 +15,33 @@ interface Section{
   description: string;
   items: Item[];
 }
-const sections: Section[] = [
+
+const { t, tm, rt } = useI18n()
+const localePath = useLocalePath();
+
+const sections = computed<Section[]>(() => [
   {
-    title: "Applications",
-    description: "The list of currently deployed applications",
+    title: t("applicationsTitle"),
+    description: t("applicationsDescription"),
     items: [
       {
-        title: "PDF Extractor",
-        description: "Easily extract text, images, and data from PDF files with a fast, user-friendly tool. Simplify document processing on any device.",
-        link: '/pdf-extractor',
-        image: '/images/pdf-extractor.jfif',
-      },
-      {
-        title: "Language Learning Platform",
-        description: "Master languages (RU-EN, JA-EN, FR-EN) with quiz and view modes. Track progress and learn anywhere via Telegram.",
-        link: '/learn-language-bot',
+        title: t("learnLanguageBotTitle"),
+        description: t("learnLanguageBotDescription"),
+        link: localePath('/learn-language-bot'),
         image: '/images/learn-language.jpg',
       },
       {
-        title: "Advertisements Collector",
-        description: "This application collects and displays advertisements from various sources and ranking them with AI.",
-        link: '/crawled-apartments',
+        title: t("advertisementsCollectorTitle"),
+        description: t("advertisementsCollectorDescription"),
+        link: localePath('/crawled-apartments'),
         image: '/images/crawled-advertisements.png',
-      }
+      },
+      {
+        title: t("pdfExtractorTitle"),
+        description: t("pdfExtractorDescription"),
+        link: localePath('/pdf-extractor'),
+        image: '/images/pdf-extractor.jfif',
+      },
     ]
   },
   {
@@ -47,38 +51,78 @@ const sections: Section[] = [
       {
         title: "Laraue.EfCoreTriggers",
         description: "Allows to work with triggers via C# code.",
-        link: '/blog/projects/ef-core-triggers',
+        link: localePath("/blog/projects/ef-core-triggers"),
         tags: [".NET", "EF Core", "Database Triggers"]
       },
       {
         title: "Laraue.PdfQL",
         description: "PDF Query Language - A powerful language for querying and manipulating PDF documents.",
-        link: '/blog/projects/pdf-query-language',
+        link: localePath('/blog/projects/pdf-query-language'),
         tags: [".NET", "PDF", "Interpreter", "Query language"]
       },
       {
         title: "Laraue.Crawling",
         description: "A robust and flexible web crawling library for .NET.",
-        link: '/blog/projects/crawler',
+        link: localePath('/blog/projects/crawler'),
         tags: [".NET", "Web Crawling", "Scraping"]
       },
       {
         title: "Laraue.Telegram.NET",
         description: "Work with telegram via Telegram controllers.",
-        link: '/blog/projects/telegram-net',
+        link: localePath('/blog/projects/telegram-net'),
         tags: [".NET", "Telegram Bot API"]
       }
     ]
   }
-]
+])
+
 </script>
+
+<i18n lang="json">
+{
+  "en": {
+    "devName": "Ilya Belyansky",
+    "devDescription": "Backend Developer",
+    "aboutTitle": "About Resource",
+    "aboutDescription": [
+      "I am a software engineer with 8+ years experience that was born in Russia. The technologies I often use are C# + Postgres + Vue.",
+      "The Website is a platform where I demonstrate my projects, and it is also a Blog where I tell how I was implementing them."
+    ],
+    "applicationsTitle": "Applications",
+    "applicationsDescription": "The entire list of applications",
+    "pdfExtractorTitle": "PDF to JSON Converter",
+    "pdfExtractorDescription": "Easily extract text, images, and data from PDF files with a fast, user-friendly tool.",
+    "learnLanguageBotTitle": "Language Learning Bot",
+    "learnLanguageBotDescription": "Master languages (RU-EN, JA-EN, FR-EN) with flashcards. Track progress and learn anywhere via Telegram.",
+    "advertisementsCollectorTitle": "AI Apartments Aggregator",
+    "advertisementsCollectorDescription": "This application collects and displays advertisements from various sources and ranking them with AI."
+  },
+  "ru": {
+    "devName": "Белянский Илья",
+    "devDescription": "Backend разработчик",
+    "aboutTitle": "О сайте",
+    "aboutDescription": [
+      "Я - разработчик ПО из России с более чем 8-ми летним опытом. Мой базовый стек - C# + Postgres + Vue.",
+      "Данный сайт является платформой для демонстрации реализованных проектов и блогом, где я рассказываю о том, как эти проекты создавались."
+    ],
+    "applicationsTitle": "Приложения",
+    "applicationsDescription": "Список доступных приложений",
+    "pdfExtractorTitle": "Конвертер PDF в JSON",
+    "pdfExtractorDescription": "Извлеките текст, изображения, таблицы из PDF с помощью данной утилиты.",
+    "learnLanguageBotTitle": "Бот для изучения языков",
+    "learnLanguageBotDescription": "Пополняйте словарный запас (RU-EN, JA-EN, FR-EN) с помощью квизов. Отслеживайте прогресс и обучайтесь в любом месте через Telegram.",
+    "advertisementsCollectorTitle": "ИИ аггрегатор недвижимости",
+    "advertisementsCollectorDescription": "Приложение собирает объявления с разных платформ недвижимости и делает их ранжирование с помощью ИИ"
+  }
+}
+</i18n>
 
 <template>
   <div class="container">
     <header class="hero">
       <div class="hero-content">
-        <h1 class="hero-title">Ilya Belyansky</h1>
-        <p class="hero-subtitle">Backend Developer</p>
+        <h1 class="hero-title">{{ t('devName') }}</h1>
+        <p class="hero-subtitle">{{ t('devDescription') }}</p>
         <div class="hero-decorator"></div>
       </div>
     </header>
@@ -86,13 +130,11 @@ const sections: Section[] = [
     <main class="main-content">
       <section class="about-section">
         <div class="section-header">
-          <h2 class="section-title">About Resource</h2>
+          <h2 class="section-title">{{ t('aboutTitle') }}</h2>
           <div class="section-divider"></div>
         </div>
         <div class="about-text">
-          <p>I am yet another one software engineer that was born in Russia. I like to explore different concepts and technologies, and this site serves as a platform where I share my ideas and projects.</p>
-          <p>The site contains my thoughts, tutorials, and the development journey of various projects I've worked on in the <nuxt-link to="/blog" class="link-highlight">Blog section</nuxt-link>. I made this resource to structure all I ever made and provide a reference for other developers who want to create something useful.</p>
-          <p>The resource also contain some deployed apps. To access them I suggest exploring the links above.</p>
+          <p v-for="paragraph in tm('aboutDescription')">{{ rt(paragraph as any) }}</p>
         </div>
       </section>
 
