5af1cf86be6d06de7bb67759f0b8971a29ed6578
Ilya Belyansky <win7user10@yandex.ru>
2026-03-15 19:47:09 +0300

    update links

--- app/pages/note-board-bot.vue
+++ app/pages/note-board-bot.vue
@@ -591,7 +591,7 @@ useSeoMeta({
             <div class="pf pf-x"><div class="pf-check">✕</div><span>{{ t('featureAIFree') }}</span></div>
             <div class="pf pf-x"><div class="pf-check">✕</div><span>{{ t('featureExportFree') }}</span></div>
           </div>
-          <a href="https://t.me/note_board_bot" class="btn-price">{{ t('startFree') }}</a>
+          <a target="_blank" rel="nofollow" :href="'https://t.me/' + botName" class="btn-price">{{ t('startFree') }}</a>
         </div>
         <div class="price-card featured">
           <div class="price-popular">{{ t('mostPopular') }}</div>
@@ -608,7 +608,7 @@ useSeoMeta({
             <div class="pf"><div class="pf-check">✓</div><span>{{ t('featureAIPremium') }}</span></div>
             <div class="pf"><div class="pf-check">✓</div><span>{{ t('featureExportPremium') }}</span></div>
           </div>
-          <a href="https://t.me/note_board_bot" class="btn-price">{{ t('getPremium') }}</a>
+          <a target="_blank" rel="nofollow" :href="'https://t.me/' + botName" class="btn-price">{{ t('getPremium') }}</a>
         </div>
       </div>
     </div>
@@ -666,7 +666,7 @@ useSeoMeta({
       <div class="section-label">{{ t('ctaLabel') }}</div>
       <h2 class="section-title">{{ t('ctaTitle') }}</h2>
       <p class="section-sub">{{ t('ctaSub') }}</p>
-      <a href="https://t.me/note_board_bot" class="btn-primary" style="display:inline-flex;">
+      <a target="_blank" rel="nofollow" :href="'https://t.me/' + botName" class="btn-primary" style="display:inline-flex;">
         <svg width="18" height="18" fill="currentColor" viewBox="0 0 24 24"><path d="M12 0C5.374 0 0 5.373 0 12c0 6.628 5.374 12 12 12 6.629 0 12-5.372 12-12C24 5.373 18.629 0 12 0zm5.562 8.247l-2.01 9.465c-.147.658-.537.818-1.084.508l-3-2.21-1.447 1.394c-.16.16-.295.295-.605.295l.215-3.053 5.56-5.023c.242-.213-.054-.333-.373-.12L7.48 14.52l-2.95-.924c-.64-.2-.652-.64.135-.948l11.547-4.452c.533-.194 1.001.13.35.051z"/></svg>
         {{ t('openBot', { botName: '@' + botName }) }}
       </a>
